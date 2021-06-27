feee9d0a51617c89c6ee848836afead74f375264
Jamal135 <50566368+Jamal135@users.noreply.github.com>
2021-06-27 11:11:13 +1000

    CSV build setup minor edits.

--- scrape.py
+++ scrape.py
@@ -20,29 +20,29 @@ def grabHTML(url, start):
     webpage = lib.urlopen(req)
     return BeautifulSoup(webpage, 'html.parser')
 
+# Function buildCSV
+def buildCSV():
+    ''' Returns: Built and named CSV file containing organisation review data. '''
+    pass
+
 # Function: reviewScrape
 def reviewScrape(url, outputName):
     ''' Returns: Review data for Indeed.com organisation as CSV with output name. '''
-
     # Grab first page.
     globalSoup = grabHTML(url, 0)
-
     # Extract number pages.
     reviewsPerPage = 20
     overviewData = globalSoup.find('div', attrs={'itemprop': 'aggregateRating'})
     numberReviews = int(overviewData.find('meta').attrs['content'])
     numberPages = ceil(numberReviews - 1 / reviewsPerPage)
     numberPages = 1 # Temp to limit requests
-
     # Iterate through each page of reviews.
     for page in range(numberPages):
-
         # Sleep randomly first.
         sleep(randint(0, 3))
         # After first page, ignore featured review and update HTML.
         if page == 0: soup, jump = globalSoup, 0
         else: soup, jump = grabHTML(url, page * reviewsPerPage), 1
-
         # Extract positions data and author text.
         authorText = []
         for authorData in soup.find_all('span', attrs={'itemprop': 'author'})[jump:]:
@@ -64,5 +64,6 @@ def reviewScrape(url, outputName):
         # Extract review data.
         for reviewData in soup.find_all('span', attrs={'itemprop': 'reviewBody'})[jump:]:
             reviews.append(reviewData.text)
+    # Build resulting CSV file.
 
 reviewScrape(getenv('URL'), getenv('NAME'))
