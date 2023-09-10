f272a9a837bbb3fa2fbf1c9aa9c95ede9f720b3b
Jamal135 <50566368+Jamal135@users.noreply.github.com>
2023-09-10 15:38:47 +1000

    Fully fleshed out error handling, added load check

--- seek_scraper.py
+++ seek_scraper.py
@@ -7,27 +7,46 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from time import sleep
 import json
 from bs4 import BeautifulSoup
 from tqdm import tqdm
 from utility import Log
 import re
-import traceback
+import os
 
-PATTERN = r'https?://www\.seek\.com\.au/companies/.+/reviews'
+URL_PATTERN = re.compile(r'https?://www\.seek\.com\.au/companies/.+/reviews')
+NAME_PATTERN = re.compile(r'^[a-zA-Z0-9\s\-.,()]+$')
+YEAR_PATTERN = re.compile(r"\d{4}")
+
+WEBDRIVER_TIMEOUT = 20
+DATA_START_OFFSET = 5
+DATA_LENGTH = 9
+DATA_YEAR_IDX = 1
+DATA_GOOD_TEXT = 'The good things'
+DATA_CHALLENGE_TEXT = 'The challenges'
+DATA_CHALLENGE_IDX = 7
+
+class InvalidJsonFormat(Exception):
+    pass
+
+class UnexpectedData(Exception):
+    pass
 
 class Config_JSON:
     ''' Purpose: Load specified scrape_config contents. '''
-    # Missing code to verify JSON file contents/existence
     def __init__(self, json_file_path):
         self.orgs = []
-        self.load_orgs(json_file_path)
-    def load_orgs(self, json_file_path):
-        ''' Purpose: Creates organisations, list of URL and name dictionaries. '''
+        if not os.path.exists(json_file_path):
+            raise FileNotFoundError(f'{json_file_path} does not exist.')
         with open(json_file_path, 'r') as file:
             data = json.load(file)
-            for name, url in data['organisations'].items():
+            if 'orgs' not in data:
+                raise InvalidJsonFormat('JSON is missing the "orgs" key...')
+            for name, url in data['orgs'].items():
+                if not URL_PATTERN.match(url):
+                    raise InvalidJsonFormat(f'JSON contains invalid URL format: {url}')
+                if not NAME_PATTERN.match(name):
+                    raise InvalidJsonFormat(f'JSON contains invalid name format: {name}')
                 self.orgs.append({'name': name, 'url': url})
     def get_orgs(self) -> list:
         ''' Returns: List of organisation names and URLs. '''
@@ -36,99 +55,141 @@ class Config_JSON:
         ''' Returns: String of organisation names and URLs. '''
         return '\n'.join([f'{org["name"]}: {org["url"]}' for org in self.orgs])
 
-def create_browser(debug: bool = False) -> webdriver:
+def create_browser(header: bool = False, logging: bool = False) -> webdriver:
     ''' Returns: Created Selenium Chrome browser session. '''
     options = webdriver.ChromeOptions()
-    if not debug:
+    if not header:
         options.add_argument('--headless')
-    options.add_experimental_option('excludeSwitches', ['enable-logging']) # Comment for logs
+    if not logging:
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])
     driver = webdriver.Chrome(
         service=Service(ChromeDriverManager().install()), 
         options=options)
     driver.implicitly_wait(10)
     return driver
 
-def extract_data(page_html: str) -> list:
+def extract_data(page_html: str, data_strict: bool) -> list:
     ''' Returns: List of lists of all reviews data scraped for current page.'''
     soup = BeautifulSoup(page_html, 'html.parser')
     texts = [element.get_text() for element in soup.find_all(['span', 'h3'])]
     data = []
-    indices = [i for i, x in enumerate(texts) if x == 'The good things']
+    # Finds 'The good things' in all texts then extracts from each index.
+    indices = [i for i, x in enumerate(texts) if x == DATA_GOOD_TEXT]
+    # Expect: ['position', 'month year', 'location', 'time in role, employee status', 'title', 'The good things', 'pros', 'The challenges', 'cons']
     for idx in indices:
-        if idx - 5 >= 0 and idx + 3 < len(texts):
-            data.append(texts[idx - 5: idx + 4])
+        try:
+            start_idx = idx - DATA_START_OFFSET
+            end_idx = idx + DATA_LENGTH - DATA_START_OFFSET
+            if not (start_idx >= 0 and end_idx < len(texts)):
+                raise UnexpectedData(f'Expected data block goes out of bounds:\n{texts}')
+            block = texts[start_idx:end_idx]
+            if not YEAR_PATTERN.match((block[DATA_YEAR_IDX].split()[1])):
+                raise UnexpectedData(f'Expected year at second block index:\n{block}')
+            if not block[DATA_CHALLENGE_IDX] == DATA_CHALLENGE_TEXT:
+                raise UnexpectedData(f'Expected challenge text at second last block index:\n{block}')
+            data.append(block)
+        except UnexpectedData as e:
+            Log.alert(f'Potential bad data!\n{e.args[0]}')
+            if data_strict:
+                Log.warn(f'Settings on data_strict True, aborting...')
+                raise UnexpectedData
+            else:
+                Log.warn(f'Settings on data_strict False, proceeding...')
+                continue
     return data
 
 def next_page(next_button: webdriver.Remote._web_element_cls) -> bool:
     ''' Returns: Boolean True or False if next page is accessible. '''
     return next_button.get_attribute("tabindex") != "-1"
 
-def scrape_data(driver: webdriver.Chrome) -> list:
+class wait_for_change(object):
+    ''' Purpose: Verifies new reviews have loaded by span text changes. '''
+    def __init__(self, driver: webdriver.Chrome, data_strict: bool, element_locator):
+        self.driver = driver
+        self.locator = element_locator
+        self.data_strict = data_strict
+    def __enter__(self):
+        self.old_texts = [elem.text for elem in self.driver.find_elements(*self.locator)]
+        return self
+    def content_has_changed(self, driver: webdriver.Chrome) -> bool:
+        current_texts = [elem.text for elem in driver.find_elements(*self.locator)]
+        return current_texts != self.old_texts
+    def __exit__(self, *_):
+        try:
+            wait = WebDriverWait(self.driver, WEBDRIVER_TIMEOUT)
+            wait.until(lambda driver: self.content_has_changed(driver))
+        except TimeoutException as e:
+            Log.alert('Potential bad data!\nPage changed but review content did not...')
+            if self.data_strict:
+                Log.warn(f'Settings on data_strict True, aborting...')
+                raise UnexpectedData
+            else:
+                Log.warn(f'Settings on data_strict False, proceeding...')
+                pass
+
+def scrape_data(driver: webdriver.Chrome, data_strict: bool) -> list:
     ''' Returns: List of lists of all reviews data scraped for current URL. '''
     pbar = tqdm(total=0)
     review_data = []
     while True:
         page_html = driver.page_source
-        review_data.extend(extract_data(page_html))
+        review_data.extend(extract_data(page_html, data_strict))
         next_button = driver.find_element(By.XPATH, "//a[@aria-label='Next']")
         if next_page(next_button):
-            next_button.click()
-            wait = WebDriverWait(driver, 30)
-            wait.until(lambda driver: driver.page_source != page_html)
-            sleep(3) # Find more robust next page verification
             pbar.update(1)
+            with wait_for_change(driver, data_strict, (By.TAG_NAME, 'h3')):
+                next_button.click()
         else:
             pbar.close()
             break
     Log.status(f'Extracted {len(review_data)} reviews')
     return review_data
 
-def scrape_seek(driver: webdriver.Chrome, config_json: Config_JSON):
+def scrape_seek(driver: webdriver.Chrome, config_json: Config_JSON, data_strict: bool):
     ''' Purpose: Control Selenium to extract organisation reviews across pages. '''
     failed = []
     for name, url in config_json.get_orgs():
         Log.status(f'Scraping {name}')
         try:
-            if not re.match(PATTERN, url):
-                Log.alert(f'Provided URL is not a valid Seek company reviews link...\n{url}')
+            driver.get(url)
+            wait = WebDriverWait(driver, WEBDRIVER_TIMEOUT)
+            wait.until(EC.presence_of_element_located((By.XPATH, "//a[@aria-label='Next']")))
+            review_data = scrape_data(driver, data_strict)
+            review_element = driver.find_element(By.XPATH, f"//a[contains(@href, '{url.split('/')[-2]}')]")
+            total_reviews = int(review_element.text.split()[0])
+            if len(review_data) != total_reviews:
+                Log.alert(f'Expected {total_reviews}, got {len(review_data)} for {name}, check internet...')
                 failed.append(name)
                 continue
-            driver.get(url)
-            wait = WebDriverWait(driver, 20)
-            wait.until(EC.presence_of_element_located((By.XPATH, "//a[@aria-label='Next']")))
+            # add code to save data
         except TimeoutException:
             Log.alert(f'Failed to get {name}, check URL or internet...\n{url}')
             failed.append(name)
             continue
-        #try:
-        review_data = scrape_data(driver)
-            # Add way to source total_reviews
-            # if len(review_data) != total_reviews:
-            #     Log.alert(f'Mismatch between expected and actual {name} reviews...')
-            #     failed.append(name)
-            #     continue
-        #except: # Incomplete, requires explicit exception handling
-        #    pass        
 
-def scrape_launch(scrape_file: str, debug: bool = False):
+def scrape_launch(scrape_file: str, data_strict:bool = True, selenium_header: bool = False, selenium_logging: bool = False):
     ''' Purpose: Creates driver and data objects for scraping. '''
     driver = None
     try:
-        driver = create_browser(debug)
+        driver = create_browser(header=selenium_header, logging=selenium_logging)
         config_json = Config_JSON(scrape_file)
         Log.info(f'Loaded {scrape_file} contents:\n{config_json.string()}')
-        scrape_seek(driver, config_json)
+        scrape_seek(driver, config_json, data_strict)
+    except KeyboardInterrupt:
+        Log.warn('Keyboard interrupt, aborting...')
+    except (FileNotFoundError, InvalidJsonFormat) as e:
+        Log.alert(f'{e.args[0]}\nFailed to load {scrape_file}, aborting...')
+    except UnexpectedData:
+        Log.trace(e.__traceback__)
     except Exception as e:
-        if isinstance(e, KeyboardInterrupt):
-            Log.alert('Keyboard interrupt, aborting...')
-        else:
-            Log.alert(f'Unexpected error occurred...\n{e}')
-            traceback.print_exc()
+        Log.alert(f'Unexpected error occurred...\n{e}')
+        Log.trace(e.__traceback__)
     finally:
         if driver:
+            Log.status('Killing Selenium driver...')
             driver.quit()
-        exit(0)
+        quit()
 
 if __name__ == '__main__':
     Log.info('Debug test, browser has header...')
-    scrape_launch('scrape_configs/test.json', debug=True)
+    scrape_launch('scrape_configs/test.json', selenium_header=True)
--- utility.py
+++ utility.py
@@ -1,20 +1,34 @@
 ''' Creation Date: 09/11/2022 '''
 
 from colorama import Fore, Style
+import traceback
 
-# Stores general classes and functions used across project
 
 class Log:
     ''' Purpose: Correctly format print messages given purpose. '''
+    PREFIX_STATUS = f'[{Fore.GREEN}-{Style.RESET_ALL}]'
+    PREFIX_WARN = f'{Fore.LIGHTBLACK_EX}[{Fore.CYAN}*{Style.RESET_ALL}{Fore.LIGHTBLACK_EX}] {Fore.CYAN}Warning:{Style.RESET_ALL}'
+    PREFIX_ALERT = f'{Fore.LIGHTBLACK_EX}[{Fore.RED}!{Style.RESET_ALL}{Fore.LIGHTBLACK_EX}] {Fore.RED}ERROR:{Style.RESET_ALL}'
+    PREFIX_INFO = f'[{Fore.BLUE}i{Style.RESET_ALL}{Fore.LIGHTBLACK_EX}]'
+    PREFIX_TRACE = f'{Fore.LIGHTBLACK_EX}[{Fore.RED}!{Style.RESET_ALL}{Fore.LIGHTBLACK_EX}] {Fore.RED}TRACE:{Style.RESET_ALL}'
+    @staticmethod
     def status(message: str):
-        ''' Format: [-] message'''
-        indicator = '[' + Fore.GREEN + '-' + Style.RESET_ALL + ']'
-        print(f'{indicator} {message}')
+        ''' Format: [-] message '''
+        print(f'{Log.PREFIX_STATUS} {message}')
+    @staticmethod
+    def warn(message: str):
+        ''' Format: [*] Warning: message '''
+        print(f'{Log.PREFIX_WARN} {Fore.LIGHTBLACK_EX}{message}{Style.RESET_ALL}')
+    @staticmethod
     def alert(message: str):
-        ''' Format: [!] message'''
-        indicator = '[' + Fore.RED + '!' + Style.RESET_ALL + ']'
-        print(f'{indicator} {message}')
+        ''' Format: [!] ERROR: message '''
+        print(f'{Log.PREFIX_ALERT} {Fore.LIGHTBLACK_EX}{message}{Style.RESET_ALL}')
+    @staticmethod
     def info(message: str):
-        ''' Formats: [i] message'''
-        indicator = '[' + Fore.BLUE + 'i' + Style.RESET_ALL + ']'
-        print(f'{indicator} {message}')
+        ''' Formats: [i] message '''
+        print(f'{Log.PREFIX_INFO} {message}{Style.RESET_ALL}')
+    @staticmethod
+    def trace(error_traceback):
+        ''' Formats: [!] trace '''
+        formatted_traceback = ''.join(traceback.format_tb(error_traceback))
+        print(f'{Log.PREFIX_TRACE}\n{Fore.LIGHTBLACK_EX}{formatted_traceback}{Style.RESET_ALL}')
